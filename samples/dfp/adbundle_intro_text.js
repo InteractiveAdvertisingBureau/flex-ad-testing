fc20a1deb09b1b24a6e9e9a22fd2ff4fbb140f66
Chris Cole <goosemanjack@gmail.com>
2017-07-05 15:41:46 -0700

    Style tweaks and layout tweaks.

---
+++ samples/dfp/adbundle_intro_text.js
@@ -0,0 +1,13 @@
+/**
+ * ...
+ * @author chris cole
+ */
+
+(function() {
+	
+	document.write('<p>\n'
++ 'This page demonstrates a flexible sized ad container implementation serving different types of ads that can be expected during the transition period for this ad unit. The ads are being served using standard Google DFP GPT tags and ad delivery setup and features in combination with supported creative types, (i.e., flexible sized ad containers). In this test, ad tags and ad images have been provided by creative platforms Flexitive, Responsive Ads, and Flashtalking which are examples of supported creative types.'
++ '</p>\n\n');
+
+	
+})();
